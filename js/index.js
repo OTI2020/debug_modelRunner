ddf2c78c32752f49608e5f5e5724610ae6b94b50
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-03-27 19:33:27 +0100

    delete trash

--- js/index.js
+++ js/index.js
@@ -2,20 +2,8 @@
 
 try {
     const runner = new Models.ModelRunner();
+    console.log(`init ModelRunner: ${JSON.stringify(runner, null, 10)}`)
     runner.init();
-    let counter = 0
-    function runSimulation() {
-        console.log("Iteration:" + counter);
-        runner.step();
-        // Check for termination condition (e.g., number of steps)
-        counter++
-        final_step = runner.parameters.SIMULATION.steps
-        console.log(`final_step = ${final_step}`);
-        if (counter < 5) {
-            runSimulation();
-        }
-    }
-    runSimulation()
 } catch (error) {
     console.error(`Error in html script: ${error.message}`);
 }
