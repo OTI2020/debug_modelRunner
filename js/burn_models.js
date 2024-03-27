d1e9b75746c77968992a044872926c736926159d
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-03-27 18:04:52 +0100

    added try catch

--- js/burn_models.js
+++ js/burn_models.js
@@ -10,13 +10,18 @@ window.Models.SimpleBurnModel = (SimpleBurnModel = class SimpleBurnModel {
         this.burn_time = 1;
     }
     simulate_burn(point, t0, t1, t_resolution) {
-        if (!point.is_ignited(t0, t1)) {
-            return;
-        }
-        point.fuel_load || (point.fuel_load = this.burn_time);
-        point.fuel_load = point.fuel_load - 1;
-        if (point.fuel_load === 0) {
-            return point.extinguish_time = point.ignition_time + this.burn_time * t_resolution;
+        try {
+            if (!point.is_ignited(t0, t1)) {
+                return;
+            }
+            point.fuel_load || (point.fuel_load = this.burn_time);
+            point.fuel_load = point.fuel_load - 1;
+            if (point.fuel_load === 0) {
+                return point.extinguish_time = point.ignition_time + this.burn_time * t_resolution;
+            }
+
+        } catch (error) {
+            console.error(`simulate_burn SimpleBurn;odel: ${error.message}`);
         }
     }
 })
