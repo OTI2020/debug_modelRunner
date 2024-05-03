0955e0472c821a8eac608fc97e205437525cc9a8
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2024-05-03 15:50:34 +0200

    fixed table for testing

--- js/index.js
+++ js/index.js
@@ -14,8 +14,8 @@ console.log("number of objects in the grid: " + runner.grid.length * runner.grid
 // console.log("first line in grid: " + runner.grid) //[0]);
 console.log("first point in grid: " + runner.grid[0][0]);
 console.log("first index in grid: " + JSON.stringify(runner.grid[0][0].index));
-console.log("first position in grid: " + JSON.stringify(runner.grid[0][0].position));
-console.log("first runner in grid: " + runner.grid[0][0].runner);
+console.log("first position in grid: " + JSON.stringify(runner.grid[0][0].position) + "(1/1) position in grid: " + JSON.stringify(runner.grid[1][1].position));
+console.log("first runner in grid: " + JSON.stringify(runner.grid[0][0].runner.parameters));
 console.log("first ignition_time in grid: " + JSON.stringify(runner.grid[0][0].ignition_time));
 console.log("next ignition_time in grid: " + JSON.stringify(runner.grid[2][2].ignition_time));
 console.log("last ignition_time in grid: " + JSON.stringify(runner.grid[4][4].ignition_time));
@@ -26,3 +26,28 @@ console.log("first neighbours in grid: " + runner.grid[0][0].neighbours);
 
 console.timeEnd();
 
+
+/////////////
+// fill each ignited cell of table with arrival time 
+/////////////
+
+// DOM interaction
+function fill_table(in_list) {
+    let debug_counter = 0
+    for (let i = 0; i < in_list.length; i++) {
+        console.log("i = " + i);
+        console.log("test DOM: " + document.getElementById("result_table"));
+        console.log("test DOM: " + document.getElementById("x0y0").value);
+        console.log("test DOM: " + document.getElementById("x1y1").value);
+        console.log("test DOM: " + document.getElementById("x1y1").textContent);
+
+        // Code, der den Fehler auslösen kann
+        let table_cell = document.getElementById(`x${in_list[i].index.x}y${in_list[i].index.y}`); // uses id of needed cell in the table
+        let roundedNumber = Math.round(in_list[i].ignition_time * 1000) / 1000
+        console.log("table_cell " + table_cell + ` x${in_list[i].index.x}y${in_list[i].index.y}`);
+        table_cell.textContent = roundedNumber  // in_list[i].id;
+
+        debug_counter++
+    }
+    // set_color_thresholds(in_list)
+}
